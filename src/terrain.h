8fcadc720e89aed3c372d76482cdd365899693d5
jamesminardi <jamesminardip@gmail.com>
2024-02-05 23:44:41 -0600

    Example noise function

--- src/terrain.h
+++ src/terrain.h
@@ -7,6 +7,34 @@
 #include <webgpu/webgpu.hpp>
 
 
+
+//
+//float fbmExpensive(glm::vec2 x, float H) {
+//	float t = 0.0f;
+//	for (int i = 0; i < 3; i++) {
+//		float f = pow(2.0f, float(i));
+//		float a = pow(f, -H);
+//		t += a * valueNoise(x * f);
+//	}
+//	return t;
+//}
+//
+//float fbm(glm::vec2 x) {
+//	float G = 0.5f;
+//	float f = 1.0f;
+//	float a = 1.0f;
+//	float t = 0.0f;
+//
+//	// Each octave is twice the frequency of the previous one
+//	for (int i = 0; i < 3; i++) {
+//		t += a * valueNoise(x * f);
+//		f *= 2.0f;
+//		a *= G;
+//	}
+//	return t;
+//}
+
+
 class Mesh  {
 public:
 	std::vector<float> vertices;
@@ -30,6 +58,34 @@ public:
 
 	}
 
+	static float hash(glm::vec2 uv) {
+		return glm::fract(glm::sin(glm::dot(uv, glm::vec2(12.9898f, 78.233f))) * 43758.5453f);
+	}
+
+	static float valueNoise(glm::vec2 x) {
+		glm::vec2 i = glm::floor(x);
+		glm::vec2 f = glm::fract(x);
+
+		// Smoothstep
+		glm::vec2 u = f * f * (3.0f - 2.0f * f);
+
+		// Hash coordinates
+		glm::vec2 a = glm::vec2(i.x, i.y);
+		glm::vec2 b = glm::vec2(i.x + 1.0f, i.y);
+		glm::vec2 c = glm::vec2(i.x, i.y + 1.0f);
+		glm::vec2 d = glm::vec2(i.x + 1.0f, i.y + 1.0f);
+
+		// Hash values
+		float v1 = hash(a);
+		float v2 = hash(b);
+		float v3 = hash(c);
+		float v4 = hash(d);
+
+		// Interpolate
+		float x1 = glm::mix(v1, v2, u.x);
+		float x2 = glm::mix(v3, v4, u.x);
+		return glm::mix(x1, x2, u.y);
+	}
 
 	// Generates vertices from left to right, bottom to top
 	static std::vector<float> generateGridVertices(int numCells, float scale) {
@@ -39,8 +95,8 @@ public:
 		for (int i = 0; i <= numCells; i++) {
 			for (int j = 0; j <= numCells; j++) {
 				float x = (j - halfSize) * scale;
-				float y = 0.0f;
 				float z = (i - halfSize) * scale;
+				float y = valueNoise(glm::vec2(i, j)) * 5.0f;
 
 				vertices.push_back(x);
 				vertices.push_back(y);
